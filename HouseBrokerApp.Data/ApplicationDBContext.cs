877d7acafd388bd88c0086b9ef5e9ea20734869b
brajiet <rajitbhusal@yahoo.com>
2024-02-27 16:33:25 +0545

    repo

---
+++ HouseBrokerApp.Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HouseBrokerApp.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        // DbSet properties here
+    }
+}
--- HouseBrokerApp.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace HouseBrokerApp.Domain
-{
-    public class Class1
-    {
-        public int Id { get; set; }
-    }
-}
